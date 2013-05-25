a80637517566088b90c6a50f40ee2e45ee664eb5
John Maxwell <john@musicglue.com>
2013-05-25 22:58:35 +0100

    fix broken specs on older ruby versions

--- spec/lib/truss/router/node_spec.rb
+++ spec/lib/truss/router/node_spec.rb
@@ -14,37 +14,37 @@ describe Truss::Router::Node do
     describe "routing regexes" do
         context "for get requests" do
             subject { Truss::Router::Node.new(:get, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(GET|HEAD|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(GET|HEAD|OPTIONS)/home\Z]) }
         end
 
         context "for post requests" do
             subject { Truss::Router::Node.new(:post, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(POST|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(POST|OPTIONS)/home\Z]) }
         end
 
         context "for put requests" do
             subject { Truss::Router::Node.new(:put, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(PUT|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(PUT|OPTIONS)/home\Z]) }
         end
         
         context "for patch requests" do
             subject { Truss::Router::Node.new(:patch, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(PATCH|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(PATCH|OPTIONS)/home\Z]) }
         end
         
         context "for head requests" do
             subject { Truss::Router::Node.new(:head, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(HEAD|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(HEAD|OPTIONS)/home\Z]) }
         end
         
         context "for options requests" do
             subject { Truss::Router::Node.new(:options, "/home", app) }
-            its(:matchable_regex) { should eq(/\AOPTIONS\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\AOPTIONS/home\Z]) }
         end
         
         context "for delete requests" do
             subject { Truss::Router::Node.new(:delete, "/home", app) }
-            its(:matchable_regex) { should eq(/\A(DELETE|OPTIONS)\/home\Z/) }
+            its(:matchable_regex) { should eq(%r[\A(DELETE|OPTIONS)/home\Z]) }
         end
     
     end
